2b9308d12e0da9837c474f1839c1ec8597712f67
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2024-09-25 18:07:34 +0000

    Call google_set_hostname on openSUSE and when the agent is configured to manage hostname and FQDN, let it. (#75)
    * When the agent is configured to manage hostname and FQDN, don't manage it directly.
    
    * Call google_set_hostname on openSUSE

---
+++ src/etc/sysconfig/network/scripts/google_up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+new_host_name=$(curl -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/hostname)
+new_ip_address=$(curl -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/ip)
+new_ip_address=$new_ip_address new_host_name=$new_host_name google_set_hostname
